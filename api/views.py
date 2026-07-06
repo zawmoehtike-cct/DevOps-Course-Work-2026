c3b7e25cfb8511a18568490ae64c78fc62fbed0f
Zaw Moe Htike <uchihamoee@gmail.com>
2026-07-06 19:46:37 +0100

    post book, searializer book

--- api/views.py
+++ api/views.py
@@ -1,9 +1,20 @@
+from .models import Book
+from .serializers import BookSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class BookView(APIView):
     def get(self, request, *args, **kwargs):
-        return Response({"hello": "django"}, status=status.HTTP_200_OK)
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response({"books": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 book_view = BookView.as_view()
