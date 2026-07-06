eccde9d7e90241a06ef2726226c050d2a2d8448b
Zaw Moe Htike <uchihamoee@gmail.com>
2026-07-06 20:33:18 +0100

    new field book

--- api/models.py
+++ api/models.py
@@ -5,4 +5,5 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    new_field_1 = models.TextField(blank=True)
     create_at = models.DateField(auto_now_add=True)
